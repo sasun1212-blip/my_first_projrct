e98f7dc33a3943bc33755dd2fe70382d08294684
sasso <sasun1212@gmail.com>
2026-01-28 00:46:12 +0200

    my_project

---
+++ Paython_traning/oop_example/animal_parent_object.py
@@ -0,0 +1,13 @@
+
+
+class AnimalParentObject():
+
+    def calculate_distance_at_parent(self, time, speed):
+        distance = time * speed
+        if distance < 10:
+            print("The animal is lazzy")
+
+        else:
+            print("the animal is ok")
+
+        return distance
